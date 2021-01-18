a827d134919cd81ee39cd16c0075634178ca405d
Justin Movitz <movitzj1@montclair.edu>
2021-01-18 13:52:47 -0500

    commit tree 36a139a83ffee520abfc46836270ce7951dbbf6b

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(interger)
+    
+    if interger < 0 || interger == 0 || interger == 1
+        return false
+    else 
+        (2..interger-1).to_a.all? do |element|
+            interger != element
+            interger % element != 0
+        end
+    end
+
+end
